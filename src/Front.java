ea224d478678bd97decb832785265bfed5194bd5
Zgmyr <zacharygmyr@gmail.com>
2024-09-08 12:35:40 -0500

    Added lookup() for non-letter/digit characters (symbols & operators), lex() which parses the lexeme & prints the corresponding token code, updated the main driver to parse through end of input file.

--- src/Front.java
+++ src/Front.java
@@ -12,7 +12,7 @@ public class Front {
     /* Global declarations */
     /* Variables */
     static int charClass;
-    static StringBuilder lexeme = new StringBuilder(); // holds each lexeme with 100 char limit
+    static StringBuilder lexeme = new StringBuilder(); // imposed 100 char limit
     static char nextChar;
     // static int lexLen; // (unnecessary: we use StringBuilder which has a length() method)
     static int token;
@@ -50,17 +50,10 @@ public class Front {
             // ORIGINAL: if ((in_fp = fopen("front.in","r")) == NULL)
             in_fp = new FileReader("data_file.txt");
 
-            // TESTING getChar()
             getChar();
-            System.out.println("nextChar = " + nextChar +
-                    "\ncharClass = " + charClass);
-            getChar();
-            System.out.println("nextChar = " + nextChar +
-                    "\ncharClass = " + charClass);
-
-            /* process contents of the data file */
-            // call upon getChar()
-            // do lex() while nextToken != EOF
+            do {
+                lex();
+            } while (nextToken != EOF);
 
             in_fp.close();
         }
@@ -72,7 +65,49 @@ public class Front {
         System.out.println("END OF PROGRAM"); // DEBUG
     }
 
+    /* lookup - a function to look up operators and parentheses and return the token */
+    public static int lookup(char ch) {
+        switch (ch) {
+            case '(':
+                addChar();
+                nextToken = LEFT_PAREN;
+                break;
+
+            case ')':
+                addChar();
+                nextToken = RIGHT_PAREN;
+                break;
+
+            case '+':
+                addChar();
+                nextToken = ADD_OP;
+                break;
+
+            case '-':
+                addChar();
+                nextToken = SUB_OP;
+                break;
+
+            case '*':
+                addChar();
+                nextToken = MULT_OP;
+                break;
+
+            case '/':
+                addChar();
+                nextToken = DIV_OP;
+                break;
+
+            default:
+                addChar();
+                nextToken = EOF;
+                break;
+        }
+        return nextToken;
+    }
+
     /* addChar - a function to add nextChar to lexeme */
+    /* imposes 100 character limit on lexeme */
     public static void addChar() {
         // ORIGINAL: if (lexLen <= 99) {
         if (lexeme.length() <= 99) {
@@ -85,6 +120,7 @@ public class Front {
     }
 
     /* getChar - a function to get the next character of input and determine its character class */
+    /* nextChar holds current character & charClass will be updated to LETTER, DIGIT, UNKNOWN, or EOF */
     public static void getChar() {
         try {
             int readChar; // temporarily holds FileReader value to determine charClass
@@ -119,11 +155,57 @@ public class Front {
     }
 
     /* lex - a simple lexical analyzer for arithmetic expressions */
-    /*public static int lex() {
+    /* parses the current lexeme & updates nextToken */
+    public static int lex() {
         // ORIGINAL: lexLen = 0;
         lexeme.setLength(0);
         getNonBlank();
-        // switch (charClass) ...
-    }*/
+        switch (charClass) {
+
+            /* parse identifiers */
+            case LETTER:
+                addChar(); /* takes current first character & adds it to lexeme */
+                getChar(); /* grabs next char updating nextChar & charClass */
+                while (charClass == LETTER || charClass == DIGIT) {
+                    addChar();
+                    getChar();
+                } /* loop terminates upon whitespace / end of lexeme */
+                nextToken = IDENT;
+                break;
+
+            /* parse integer literals */
+            case DIGIT:
+                addChar();
+                getChar();
+                while (charClass == DIGIT) {
+                    addChar();
+                    getChar();
+                }
+                nextToken = INT_LIT;
+                break;
+
+            /* Parentheses and operators */
+            case UNKNOWN:
+                lookup(nextChar);
+                getChar();
+                break;
+
+            /* EOF */
+            case EOF:
+                nextToken = EOF;
+                /* ORIGINAL:
+                * lexeme[0] = 'E';
+                * lexeme[1] = 'O';
+                * lexeme[2] = 'F';
+                * lexeme[3] = '0'; */
+                lexeme.replace(0,lexeme.length(),"EOF");
+                break;
+        } /* End of switch */
+
+        // ORIGINAL: printf("The next token is: %d, Next Lexeme is %s\n", nextToken, lexeme);
+        System.out.println("The next token is: " + nextToken + ", Next lexeme is " + lexeme);
+
+        return nextToken;
+    } /* End of function lex */
 
 }
